ad726477b4619457f08dfaa18e541e3c54d4d703
shela1216 <shela1216@gmail.com>
2022-08-19 23:18:09 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@
         data: data,
         computed: {
             canSend: function () {
-                if (this.nameValid == false ||  this.emailValid == false || this.messageValid == false) {
+                if (this.nameValid == false || this.emailValid == false || this.messageValid == false) {
                     return false
                 } else {
                     return true
@@ -31,19 +31,27 @@
         methods: {
             submitEmail: function () {
                 if (this.nameValid == false || this.emailValid == false || this.messageValid == false) return
-                    var template_params = {
-                        "user_email": this.emailText,
-                        "user_name": this.nameText,
-                        "user_tel": this.telText,
-                        "message": this.message,
-                    }
+                var template_params = {
+                    "user_email": this.emailText,
+                    "user_name": this.nameText,
+                    "user_tel": this.telText,
+                    "message": this.message,
+                }
                 var service_id = "service_u3q53vi";
                 var template_id = "template_c0cr3to";
                 var user_id = 'CE3xB7H17Ps8k2AJq';
+                var self = this;
                 emailjs.send(service_id, template_id, template_params, user_id)
                     .then(function (response) {
                         console.log('SUCCESS!', response.status, response.text);
-                        alert('已收到您的來信，將盡快與您聯繫')
+                        alert('已收到您的來信，將盡快與您聯繫');
+                        self.nameText = "";
+                        self.telText = "";
+                        self.emailText = "";
+                        self.message = "";
+                        var feedbackModal = document.getElementById('feedbackModal');
+                        var modal = bootstrap.Modal.getInstance(feedbackModal)
+                        modal.hide();
                     }, function (error) {
                         console.log('FAILED...', error);
                     });
@@ -51,10 +59,10 @@
         },
         watch: {
             nameText: function () {
-                if(this.nameText!="") {
-                    this.nameValid =true;
-                }else{
-                    this.nameValid =false;
+                if (this.nameText != "") {
+                    this.nameValid = true;
+                } else {
+                    this.nameValid = false;
                 }
 
             },
@@ -71,10 +79,10 @@
                 }
             },
             message: function () {
-                if(this.message!="") {
-                    this.messageValid =true;
-                }else{
-                    this.messageValid =false;
+                if (this.message != "") {
+                    this.messageValid = true;
+                } else {
+                    this.messageValid = false;
                 }
             },
         }
